9cdd24c1b7adf002ff3aca40caa0272b9e370279
WeYC <weycovo@hotmail.com>
2023-04-06 22:41:47 +0800

    update

--- diy-part-22-03a.sh
+++ diy-part-22-03a.sh
@@ -10,11 +10,14 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+# 切换分支
+git checkout 221fbfa
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # echo 'src-git-full lean_packages https://github.com/coolsnowwolf/packages.git;master' >> feeds.conf.default
 # echo 'src-git-full lean_luci https://github.com/coolsnowwolf/luci.git;master' >> feeds.conf.default
-echo 'src-git-full kenzo https://github.com/kenzok8/openwrt-packages;master' >> feeds.conf.default
-echo 'src-git-full small https://github.com/kenzok8/small;master' >> feeds.conf.default
+# echo 'src-git-full kenzo https://github.com/kenzok8/openwrt-packages;master' >> feeds.conf.default
+# echo 'src-git-full small https://github.com/kenzok8/small;master' >> feeds.conf.default
